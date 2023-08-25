a6bc0a8047dfb15117cbf452a657ba680433ca88
Надежда <nadin94403@gmail.com>
2023-08-26 02:03:03 +0300

    Начало

--- src/Employee.java
+++ src/Employee.java
@@ -1,13 +1,17 @@
 public class Employee {
-private String nameOfEmployee;
-private int department;
-private int salary;
-private int id;
+    private String nameOfEmployee;
+    private int department;
+    private double salary;
+    private int id;
+    private static int count;
 
-    public Employee(String nameOfEmployee,int department, int salary) {
+
+    public Employee(String nameOfEmployee, int department, int salary) {
+        count++;
+        this.id = count;
         this.nameOfEmployee = nameOfEmployee;
-        this.department=department;
-        this.salary=salary;
+        this.department = department;
+        this.salary = salary;
     }
 
     public String getNameOfEmployee() {
@@ -22,9 +26,10 @@ private int id;
         return department;
     }
 
-    public int getSalary() {
+    public double getSalary() {
         return salary;
     }
+
 
     public void setNameOfEmployee(String nameOfEmployee) {
         this.nameOfEmployee = nameOfEmployee;
@@ -41,4 +46,67 @@ private int id;
     public void setId(int id) {
         this.id = id;
     }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void printSeparator() {
+        System.out.println("====================");
+    }
+
+
+    public int id(Employee[] employees) {
+        for (int i = 0; i < id; i++) {
+            Employee employee = employees[i];
+        }
+        return count;
+    }
+
+    public static void showEmployee(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i].getId() + " " + employees[i].getNameOfEmployee() + ", " + employees[i].getDepartment() + ", " + employees[i].getSalary());
+        }
+    }
+
+    public static double sumSalary(Employee[] employees) {
+        double sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            sum = sum + employees[i].getSalary();
+
+        }
+        return sum;
+    }
+
+    public static double maxSalary(Employee[] employees) {
+        double maxSalary = 0;
+        //  double minSalary = -1;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].salary >= maxSalary) {
+                maxSalary = employees[i].salary;
+            }
+        }
+        return maxSalary;
+    }
+
+    public static double minSalary(Employee[] employees) {
+        double minSalary = employees[0].salary;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].salary <= minSalary) {
+                minSalary = employees[i].salary;
+            }
+        }
+        return minSalary;
+
+    }
+
+    public static double middleSalary(Employee[] employees) {
+        double middleSalary = sumSalary(employees) / employees.length;
+        return middleSalary;
+    }
+
+    public String toString() {
+        return nameOfEmployee;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
+
+
 public class Main {
     public static void main(String[] args) {
         Employee[] employee = new Employee[10];
-        employee[0] = new Employee("Василий Иванович Пупкин", 1, 35000);
-        employee[1] = new Employee("Анна Иосифовна Трубецкая", 4, 75000);
+        employee[0] = new Employee("Василий Иванович Пупкин", 1, 36000);
+        employee[1] = new Employee("Анна Иосифовна Трубецкая", 4, 74000);
         employee[2] = new Employee("Петр Николаевич Корабелкин", 4, 75000);
-        employee[3] = new Employee("Ирина Васильевна Иванова", 3, 65000);
-        employee[4] = new Employee("Семен Семенович Крупский", 2, 40000);
+        employee[3] = new Employee("Ирина Васильевна Иванова", 3, 66000);
+        employee[4] = new Employee("Семен Семенович Крупский", 2, 42000);
         employee[5] = new Employee("Алексей Николаевич Шабанов", 2, 40000);
         employee[6] = new Employee("Зинаида Петровна Семенова", 3, 65000);
-        employee[7] = new Employee("Давид Давидович Мартиросян", 5, 70000);
+        employee[7] = new Employee("Давид Давидович Мартиросян", 5, 79000);
         employee[8] = new Employee("Анастасия Игоревна Турцкевич", 5, 70000);
         employee[9] = new Employee("Лидия Ивановна Разумихина", 1, 35000);
 
+
+       Employee.showEmployee(employee);
+        Employee.printSeparator();
+       System.out.println("Общее количество сотрудников - " + Employee.getCount());
+        Employee.printSeparator();
+        System.out.println("Сумма затрат на зарплаты в месяц - " + Employee.sumSalary(employee));
+        Employee.printSeparator();
+        System.out.println("Максимальая зарплата - " + Employee.maxSalary(employee));
+        Employee.printSeparator();
+        System.out.println("Минимальная зарплата - " + Employee.minSalary(employee));
+        Employee.printSeparator();
+        System.out.println("Среднее значение зарплат -" +Employee.middleSalary(employee));
+        Employee.printSeparator();
 
     }
 }
